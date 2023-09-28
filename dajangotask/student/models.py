712de9d853a6690fa49dea53961c3cdf56974bef
Sakshapatil <128778313+Sakshapatil@users.noreply.github.com>
2023-09-28 11:57:55 +0530

    Add files via upload

---
+++ dajangotask/student/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class stud(models.Model):
+   date=models.DateField()
+   time=models.TimeField()  
+
+class stud2(models.Model):
+   name=models.CharField(max_length=100)
+   date=models.DateField()
+   time=models.TimeField()
